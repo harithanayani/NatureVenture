08ba6e0fedc20a4cb9fcedf3e88400f8fee5176c
Haritha Nayani <haritha.nayani25@gmail.com>
2016-10-19 20:34:38 +0300

    Initial Cycle Test

---
+++ Assets/scripts/Draghandlere.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Draghandlere : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+   Transform startParent;
+
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag new Called");
+        print(gameObject.name);
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    
+
+    #region IDragHandler implementation
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag Called");
+        // transform.position = eventData.position;
+        transform.position = Input.mousePosition;
+       
+    }
+
+    #endregion
+
+    #region IEndDragHandler implementation
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag Called");
+        print(gameObject.name);
+        
+         itemBeingDragged = null;
+         GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+         {
+             Debug.Log("Parent == startParent");
+             transform.position = startPosition;
+         } 
+    }
+
+    #endregion
+
+
+
+}
+
---
+++ Assets/scripts/Dropslot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class Dropslot : MonoBehaviour,IDropHandler {
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            Debug.Log("iuguigu");
+            print(gameObject.name);
+            print(eventData.pointerDrag.name);
+            Draghandlere.itemBeingDragged.transform.SetParent(transform);
+            //ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
+        }
+    }
+
+
+}
