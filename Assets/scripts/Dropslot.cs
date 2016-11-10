d06924ba175fa867ccc6fe564469e0b54e3d53a6
Haritha Nayani <haritha.nayani25@gmail.com>
2016-11-10 10:00:35 +0200

    End of cycle2 testing

--- Assets/scripts/Dropslot.cs
+++ Assets/scripts/Dropslot.cs
@@ -13,7 +13,34 @@ public class Dropslot : MonoBehaviour,IDropHandler {
             return null;
         }
     }
+    public string GetFlowerForSlot(GameObject gameObj)
+    {
+        string flowerObj;
+        print(gameObj.name);
 
+        if (gameObj.name == "Blueslot")
+        {
+            print("Current slot is Blueslot");
+            flowerObj = "Blueflower";
+        }
+        else if (gameObj.name == "Pinkslot")
+        {
+            print("Current slot is Pinkslot");
+            flowerObj = "Pinkflower";
+        }
+        else if (gameObj.name == "Yellowslot")
+        {
+            print("Current slot is Yellowslot");
+            flowerObj = "Yellowflower";
+        }
+        else
+        {
+            flowerObj = "UNDEFINED";
+            print("Undefined maping");
+        }
+
+        return flowerObj;
+    }
     public void OnDrop(PointerEventData eventData)
     {
         if (!item)
@@ -21,9 +48,21 @@ public class Dropslot : MonoBehaviour,IDropHandler {
             Debug.Log("iuguigu");
             print(gameObject.name);
             print(eventData.pointerDrag.name);
-            Draghandlere.itemBeingDragged.transform.SetParent(transform);
-            //ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
-        }
+            //Draghandlere.itemBeingDragged.transform.SetParent(transform);
+            print(Draghandlere.itemBeingDragged.name);
+            /* GetCurrentSlotName */
+            string currentSlotName = gameObject.name;
+            print(GetFlowerForSlot(gameObject));
+            print(currentSlotName);
+            if (Draghandlere.itemBeingDragged.name == GetFlowerForSlot(gameObject))
+            {
+                print("MATCH FOUND");
+                Draghandlere.itemBeingDragged.transform.SetParent(transform);
+                Destroy(item);
+            }
+
+            //Destroy(item);
+             }
     }
 
 
